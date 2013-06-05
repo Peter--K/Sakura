2448219eba9b61587baf595b224eba2ef986c009
Gary Ruben <gary.ruben@gmail.com>
2013-06-05 10:59:53 +1000

    Added Sakura version and set to 0.1

---
+++ version.py
@@ -0,0 +1,6 @@
+# From sorin's answer at http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+# Store the version here so:
+# 1) we don't load dependencies by storing it in __init__.py
+# 2) we can import it in setup.py for the same reason
+# 3) we can import it into your module module
+__version__ = '0.1'
